d78a5846b24ff9bcecf2e84df9b8792702e1ce44
tamuto <mutomob@gmail.com>
2023-02-27 13:57:55 +0000

    条件指定ミスの修正

--- devt/__init__.py
+++ devt/__init__.py
@@ -1,9 +1,9 @@
 import os
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 
 
 def isTest(val):
     return (
-        os.environ.get(val, 0) == 1,
+        os.environ.get(val, '0') != '1',
         f'environment is not defined: {val}'
     )
